cb83d0971d2ae67a24b74235eed5a57346641248
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-17 14:34:18 +0100

    our printf

---
+++ __print_formatted.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _printf - This function will produces output according to a format
+ * and with the neccessary parameters.
+ * @format: A string containing all the desired characters.
+ * Return: A total count of the characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	int print_chars;
+	conver_t fun_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+
+	va_list arg_lists;
+
+	if (format != '\0')
+	{
+		return (-1);
+	}
+	va_start(arg_lists, format);
+
