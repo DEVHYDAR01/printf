8014a20e301a3e920c2e7c116036069200701480
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-17 13:36:15 +0100

    putchar

---
+++ __char_write.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _write_char - This  writes the character c to stdout
+ * @c: This is the  character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _write_char(char c)
+{
+	return (write(1, &c, 1));
+}
