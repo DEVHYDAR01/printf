0808f907c0f9af2cbc0cd656b95c7d68f45cdeb3
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2023-09-17 14:55:44 +0100

    conflict retrived

--- __our_parser.c
+++ __our_parser.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * __our_parser - This will hold the main string with the necessary parameters also
- * to print a formatted string.
+ * __our_parser - This will hold the main string with the necessary
+ * parameters also to print a formatted string.
  * @format: This is the string containing all the desired characters.
- * @f_list: This ia a list of all of the posible functions.
- * @arg_list: This is a list also containing all of the argumentents passed to the program.
+ * @fun_list: This ia a list of all of the posible functions.
+ * @arg_lists: This is a list also containing all of
+ * the argumentents passed to the program.
  * Return: A total count of the characters printed.
  */
 int __our_parser(const char *format, transform_t fun_list[], va_list arg_lists)
--- main.h
+++ main.h
@@ -1,3 +1,24 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+* struct transform - This struct will define a struct for signs and functions
+*
+* @sign: This will be the  operator
+* @fun: This is the  function associated with it.
+*/
+typedef struct transform
+{
+       char *sign;
+       int (*fun)(va_list);
+}transform;
+
 int _printf(const char *format, ...);
 int __char_write(char c);
 int __our_parser(const char *format, transform_t fun_list[], va_list arg_lists);
+
+#endif
